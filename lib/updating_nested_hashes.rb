a044870e089b30fcf51d2cddf870107b5572c3f0
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 02:55:41 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -48,9 +48,8 @@ def add_characters
          ]
       }
    }
-    
-epic_tragedy[:additional_characters] = { name: "Prince Escalus" }
-epic_tragedy[:additional_characters] = { name: "Apothecary" }
-
-epic_tragedy
+epic_tragedy[:additional_characters] = [
+  { name: "Prince Escalus" },
+  { name: "Apothecary" }
+]
 end
